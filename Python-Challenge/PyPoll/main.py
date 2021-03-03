4fdaf6d550976ed445c2d799d1527a30553f3eb2
kj119 <69178236+kj119@users.noreply.github.com>
2021-03-02 20:49:32 -0800

    Add files via upload

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 import sys
 
-election_csv = os.path.join(r"..\..\uci-irv-data-pt-08-2020-u-c\02-Homework\03-Python\Instructions\PyPoll\Resources\election_data.csv")
+election_csv = os.path.join(r"Resources\election_data.csv")
 
 with open(election_csv, "r") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",")
